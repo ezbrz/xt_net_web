38e02e4c7d67efcec7d774caf104653505ad6ef2
ezbrz <54773657+ezbrz@users.noreply.github.com>
2019-11-25 13:19:47 +0400

    Include string tasks

---
+++ 01_11/01_11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter new string:");
+            string str = Console.ReadLine();
+            Console.WriteLine(MidString(str));
+
+
+            Console.ReadKey();
+        }
+
+        static string MidString(string str)
+        {
+            float wordLength = 0;
+            float wordCount = 0;
+            for (int i=0; i<str.Length; i++)
+            {
+                if (Char.IsLetter(str[i])) {
+                    while (i < str.Length && Char.IsLetter(str[i]))
+                    {
+                        wordLength++;
+                        i++;
+                    }
+                    wordCount++;
+                }
+            }
+            if (wordCount == 0) return "Words not found"; else return (wordLength / wordCount).ToString();
+        }
+    }
+}
---
+++ 01_12/01_12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter slave string:");
+            string primaryStr = Console.ReadLine();
+            Console.WriteLine("Enter master string:");
+            string secondaryStr = Console.ReadLine();
+            Console.WriteLine(DoubleChar(primaryStr, secondaryStr));
+
+            Console.ReadKey();
+        }
+
+        static string DoubleChar(string firstStr, string secondStr)
+        {
+            StringBuilder primaryStrBuilder = new StringBuilder();
+            foreach (char elem in firstStr)
+            {
+                if (secondStr.Contains(elem)) primaryStrBuilder.Append(elem,2); else primaryStrBuilder.Append(elem, 1);
+            }
+            return primaryStrBuilder.ToString();
+        }
+    }
+}
