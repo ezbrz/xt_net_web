8cf524444da2a10045f6566e1cb20d7c6ab375e5
ezbrz <54773657+ezbrz@users.noreply.github.com>
2019-11-28 02:02:25 +0400

    Edit task 02_01. Add class

--- 02_01/02_01/Program.cs
+++ 02_01/02_01/Program.cs
@@ -10,79 +10,25 @@ namespace _02_01
     {
         static void Main(string[] args)
         {
-            Round NewRound = new Round(new Point(15, 11), 8);
-            Console.WriteLine($"Round with center point X = {NewRound.CenterPoint.X}, Y = {NewRound.CenterPoint.Y}. Round area = {NewRound.RoundArea}. Circumference = {NewRound.RoundLength}. Radius = {NewRound.RoundRadius}");
-            NewRound.RoundRadius=3;
-            NewRound.CenterPoint = new Point(7,4);
-            Console.WriteLine($"Round with center point X = {NewRound.CenterPoint.X}, Y = {NewRound.CenterPoint.Y}. Round area = {NewRound.RoundArea}. Circumference = {NewRound.RoundLength}. Radius = {NewRound.RoundRadius}");
+            Console.WriteLine("Enter radius:");
+            int.TryParse(Console.ReadLine(), out int r);
+            Console.WriteLine("Enter coordinates. X and Y:");
+            int.TryParse(Console.ReadLine(), out int x);
+            int.TryParse(Console.ReadLine(), out int y);
+            try
+            {
+                Round NewRound = new Round(new Point(x, y), r);
+                Console.WriteLine(NewRound.ToString());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
             Console.ReadKey();
         }
     }
 
-    class Round
-    {
-        private double _roundRadius;
-        private Point _centerPoint;
-        private double _XDistance
-        {
-            set { if (value < 0) throw new ArgumentException("Round not in positive coordinates, Change X center point or radius"); }
-        }
-        private double _YDistance
-        {
-            set { if (value < 0) throw new ArgumentException("Round not in positive coordinates, Change Y center point or radius"); }
-        }
 
-
-        public double RoundRadius
-        {
-            get => _roundRadius;
-            set
-            {
-                if (value <= 0) throw new ArgumentException("Invalid value. Must be > 0", nameof(RoundRadius));
-                GetDistance(_centerPoint, value);
-                _roundRadius = value;
-            }
-        }
-        public double RoundArea
-        {
-            get => Math.PI * Math.Pow(_roundRadius, 2);
-        }
-
-        public double RoundLength
-        {
-            get => 2 * Math.PI * _roundRadius;
-        }
-        public Point CenterPoint
-        {
-            get => _centerPoint;
-            set
-            {
-                if (value.X <= 0 || value.Y <= 0) throw new ArgumentException("Invalid coordinates. Must be > 0", nameof(CenterPoint));
-                GetDistance(value, _roundRadius);
-                _centerPoint = value;
-            }
-        }
-        private void GetDistance(Point point, double radius)
-        {
-            _XDistance = point.X - radius;
-            _YDistance = point.Y - radius;
-        }
-        public Round(Point centerPoint, double roundRadius)
-        {
-            GetDistance(centerPoint, roundRadius);
-            CenterPoint = centerPoint;
-            RoundRadius = roundRadius;
-        }
-
-    }
-    class Point
-    {
-        public int X;
-        public int Y;
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
 }
---
+++ 02_01/02_01/Round.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_01
+{
+    class Round
+    {
+        private double _roundRadius;
+        private Point _centerPoint;
+        private double XDistance
+        {
+            set { if (value < 0) throw new ArgumentException("Round not in I quater of coordinate plane, Change X center point or radius"); }
+        }
+        private double YDistance
+        {
+            set { if (value < 0) throw new ArgumentException("Round not in I quater of coordinate plane, Change Y center point or radius"); }
+        }
+
+
+        public double RoundRadius
+        {
+            get => _roundRadius;
+            set
+            {
+                if (value <= 0) throw new ArgumentException("Invalid value. Must be > 0", nameof(RoundRadius));
+                GetDistance(_centerPoint, value);
+                _roundRadius = value;
+            }
+        }
+        public double RoundArea
+        {
+            get => Math.PI * Math.Pow(_roundRadius, 2);
+        }
+
+        public double RoundLength
+        {
+            get => 2 * Math.PI * _roundRadius;
+        }
+        public Point CenterPoint
+        {
+            get => _centerPoint;
+            set
+            {
+                if (value.X <= 0 || value.Y <= 0) throw new ArgumentException("Invalid coordinates. Must be > 0", nameof(CenterPoint));
+                GetDistance(value, _roundRadius);
+                _centerPoint = value;
+            }
+        }
+        private void GetDistance(Point point, double radius)
+        {
+            XDistance = point.X - radius;
+            YDistance = point.Y - radius;
+        }
+        public Round(Point centerPoint, double roundRadius)
+        {
+            GetDistance(centerPoint, roundRadius);
+            CenterPoint = centerPoint;
+            RoundRadius = roundRadius;
+        }
+        public override string ToString()
+        {
+            return $"Round with center point X = {CenterPoint.X}, Y = {CenterPoint.Y}. Round area = {RoundArea}. Circumference = {RoundLength}. Radius = {RoundRadius}";
+        }
+    }
+    struct Point
+    {
+        public int X;
+        public int Y;
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
